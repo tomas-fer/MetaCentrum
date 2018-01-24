d247074b4f42e36b8a94a85c680a8a82b2e687f0
Tomas Fer <tomas.fer@centrum.cz>
2018-01-24 09:20:26 +0100

    Add files via upload

---
+++ install_FastPlast_CentOS.sh
@@ -0,0 +1,13 @@
+# This script installs dependencies for FastPlast on CentOS 7 (e.g., new VM in MetaCentrum)
+# Clones FastPlast from GitHub (https://github.com/mrmckain/Fast-Plast)
+
+# Install java (necessary for Trimmomatic)
+yum install -y java-1.7.0-openjdk.x86_64
+# Install perl modules
+yum install -y perl-Env
+yum install -y perl-IO-Compress
+# Install zlib library
+yum install -y zlib-devel
+# Clone FastPlast
+git clone https://github.com/mrmckain/Fast-Plast.git
+cd FastPlast
